a24cb1f4d84b0274c170331ffac1b4f5532ff387
Alexey Koleda <alexeykoleda@icloud.com>
2022-08-03 13:51:04 +0300

    Add UIColor extension with main color theme

---
+++ QulixList/Extension/UIColor+Theme.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+Theme.swift
+//  QulixList
+//
+//  Created by Alexey Koleda on 03.08.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let theme = ColorTheme()
+    
+    struct ColorTheme {
+        let textColor = UIColor(named: "textColor")
+        let additionalTextColor = UIColor(named: "additionalTextColor")
+        
+        let lightBackgroundColor = UIColor(named: "lightBackgroundColor")
+        let darkBackgroundColor = UIColor(named: "darkBackgroundColor")
+        
+        let navigationBarColor = UIColor(named: "navigationBarColor")
+        let discountPriceColor = UIColor(named: "discountPriceColor")
+        let linkColor = UIColor(named: "linkColor")
+    }
+}
+
+
